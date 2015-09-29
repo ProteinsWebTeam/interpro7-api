5cc64cb7af55e0a7b2c2d131b115d562e8f72adb
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-09-29 17:14:48 +0100

    first attempt to connect to mysql pfam - unsuccessful

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -12,7 +12,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_see_the_home(self):
         # check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # the page title and header mention UniFam
         self.assertIn('UniFam', self.browser.title)
@@ -20,5 +20,3 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('UniFam', header_text)
 
-if __name__ == '__main__':  #7
-    unittest.main(warnings='ignore')  #8

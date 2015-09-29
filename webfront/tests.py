2cf6d9e0110d8b677a09e009c53b2a54089effdb
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-09-29 12:42:25 +0100

    First unit test and url mapping, dummy view

--- webfront/tests.py
+++ webfront/tests.py
@@ -1,10 +1,19 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from webfront.views import home_page
 
 
 class SmokeTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')  #2
-        self.assertEqual(found.func, home_page)  #3
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  #1
+        response = home_page(request)  #2
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>UniFam</title>', response.content)  #4
+        self.assertTrue(response.content.endswith(b'</html>'))  #5

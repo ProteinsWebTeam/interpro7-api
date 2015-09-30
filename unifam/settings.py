b715f3c4a335cc3de4414858cc7400df79980da8
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-09-30 11:47:50 +0100

    setting up the pfam database and including its model

--- unifam/settings.py
+++ unifam/settings.py
@@ -75,18 +75,20 @@ WSGI_APPLICATION = 'unifam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': os.path.join(BASE_DIR, 'unifam/mysql.conf')
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'unifam/mysql.conf')
+        },
+    }
+}
 
 
 # Internationalization

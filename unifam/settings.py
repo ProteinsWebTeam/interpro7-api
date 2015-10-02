2547973f15457a22f3b00dd10ccb6f273da9ccd5
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-10-02 16:24:15 +0100

    preparing for deployment

--- unifam/settings.py
+++ unifam/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'unifam.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     },
     'pfam_ro': {
         'ENGINE': 'django.db.backends.mysql',

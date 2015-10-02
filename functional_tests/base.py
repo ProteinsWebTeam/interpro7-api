6a3144891bc72971342365bd94126e1380231b97
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-10-02 14:44:10 +0100

    3 pages with their views and tests passing

---
+++ functional_tests/base.py
@@ -0,0 +1,46 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# from rest_framework.test import APIRequestFactory, APIClient
+from selenium import webdriver
+import sys
+import time
+from selenium.common.exceptions import StaleElementReferenceException
+
+class FunctionalTest(StaticLiveServerTestCase):  #1
+    @classmethod
+    def setUpClass(cls):  #1
+        for arg in sys.argv:  #2
+            if 'liveserver' in arg:  #3
+                cls.server_url = 'http://' + arg.split('=')[1]  #4
+                return  #5
+        super().setUpClass()  #6
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        # self.browser.get(self.server_url+"?testing=create")
+        # self.factory = APIRequestFactory()
+        # self.client = APIClient()
+
+    def tearDown(self):
+        # self.browser.get(self.server_url+"?testing=remove")
+        self.browser.quit()
+
+    def click_link_and_wait(self, link):
+        link.click()
+
+        def link_has_gone_stale():
+            try:
+                # poll the link with an arbitrary call
+                link.find_elements_by_id('doesnt-matter')
+                return False
+            except StaleElementReferenceException:
+                return True
+
+        self.wait_for(link_has_gone_stale)
+
--- unifam/urls.py
+++ unifam/urls.py
@@ -3,4 +3,6 @@ from webfront import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^clans/$', 'webfront.views.clans_page', name='clans_list'),
+    url(r'^clans/([a-zA-Z0-9_]+)/$', 'webfront.views.clan_page', name='view_clan'),
 ]

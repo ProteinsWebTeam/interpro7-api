6a9731cf0542e6f348231282ebb085a1ea9188bb
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-10-08 15:24:05 +0100

    moving the script in .travis to before_script

--- webfront/tests/before_install.py
+++ webfront/tests/before_install.py
@@ -4,4 +4,5 @@ for line in f:
     text += line.replace("managed = False","managed = True")
 f.close()
 f = open("webfront/models.py","w")
+print ("Config changed "+len(text.find_all("managed = True"))+ " times.")
 f.write(text)

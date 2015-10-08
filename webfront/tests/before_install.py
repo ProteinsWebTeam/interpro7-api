cfab31707e3c3179069d988a26957690e79a6c1a
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-10-08 15:42:49 +0100

    TEST_NAME in settings and removing the before install script

--- webfront/tests/before_install.py
+++
@@ -1,9 +0,0 @@
-f = open("webfront/models.py","r")
-text = ""
-for line in f:
-    text += line.replace("managed = False", "managed = True")
-f.close()
-f = open("webfront/models.py","w")
-if text.find("managed = True") != -1:
-    print("Config changed ")
-f.write(text)

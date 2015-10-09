0ae4fc57f4683ec82166f20c61a654ab0309b341
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-10-09 16:37:42 +0100

    adding a seetings template

--- webfront/tests/managed_model_test_runner.py
+++ webfront/tests/managed_model_test_runner.py
@@ -28,7 +28,7 @@ if 'test' in sys.argv or 'test_coverage' in sys.argv:  # Covers regular testing 
     DATABASES['pfam_ro']['ENGINE'] = 'django.db.backends.sqlite3'
     DATABASES['pfam_ro']['NAME'] = os.path.join(BASE_DIR, '../database/db2.sqlite3')
     DATABASES['pfam_ro']['TEST'] = {'MIRROR': 'default'}
-    del DATABASES['pfam_ro']['OPTIONS']
+    # del DATABASES['pfam_ro']['OPTIONS']
 
 # DATABASE_ROUTERS = []
 

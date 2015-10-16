ec5e1d9479dc16209d04f3ab4f4ad9443589a51a
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-10-16 14:30:49 +0100

    supporting skeleton as prefix of the URLs

--- unifam/urls.py
+++ unifam/urls.py
@@ -8,11 +8,17 @@ router.register(r'pfama', views.PFamAViewSet)
 router.register(r'clan_membership', views.MembershipViewSet)
 router.register(r'clan_relationships', views.PFamA2PFamAViewSet)
 
-urlpatterns = [
+base_urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^clans/$', 'webfront.views.clans_page', name='clans_list'),
     url(r'^clans/([a-zA-Z0-9_\-]+)/$', 'webfront.views.clan_page', name='view_clan'),
 
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
+# This is done to be able to use both the root path and the "skeleton" prefix
+urlpatterns = [
+    url('^', include(base_urlpatterns)), # iff you wish to maintain the un-prefixed URL's too
+    url('^skeleton/', include(base_urlpatterns)),
 ]

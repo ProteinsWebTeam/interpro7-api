48771ec6eb5791532b68846ddee03e083980f164
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2015-12-16 15:58:53 +0000

    commenting about the lack of migrations for interpro models

--- webfront/models/__init__.py
+++ webfront/models/__init__.py
@@ -1,4 +1,5 @@
 from webfront.models.pfam import *
 # TODO: check why tests fail when the interpro model is included
 # The problem is that that there are not migrations for this models, but there are errors when running makemigrations.
+# ../virtualenv/bin/python manage.py makemigrations -n adding_interpro_models
 # from webfront.models.interpro import *

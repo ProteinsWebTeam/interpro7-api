f841429df63fcc15a0dc908837c4322d9d30835d
Aurélien Luciani <luciani@ebi.ac.uk>
2016-03-03 21:38:36 +0000

    commit from the plane! (but not tested on the real data (yet))

--- release/management/commands/populate_db.py
+++ release/management/commands/populate_db.py
@@ -1,10 +1,11 @@
 from django.core.management.base import BaseCommand
 
-from release.iprel import get_entries
 from release.models import iprel
 from webfront.models import Entry
 
-def extractPubs(joins):
+
+# TODO: put all these functions into their own file (could that be a view?)
+def extract_pubs(joins):
     return [
         dict(
             PMID=int(p.pubmed_id), type=p.pub_type, ISBN=p.isbn,
@@ -15,6 +16,35 @@ def extractPubs(joins):
         for p in [j.pub for j in joins]
     ]
 
+def get_interpro_entries(n):
+    for input in iprel.Entry.objects.using("interpro_ro").all()[:n]:
+        output = Entry(
+            accession=input.entry_ac, type=input.entry_type_id, name=input.name,
+            short_name=input.short_name, other_names=[], member_databases={},
+            go_terms={}, literature=[extract_pubs(input.entry2pub_set.all())]
+        )
+        output.save()
+
+def get_member_db_entries(n):
+    for input in iprel.Method.objects.using("interpro_ro").all()[:n]:
+        output = Entry(
+            accession=input.method_ac, type=input.sig_type_id, name=input.name,
+            short_name='', other_names=[], member_databases={},
+            go_terms={}, literature=[]
+        )
+        output.save()
+
+# turn this into a decorator? (not important (at all))
+def log(n, kind, source='IPREL'):
+    message = '  -> Putting {n} {kind}{plural} from {source}'.format(
+        n=n, kind=kind, plural=('s' if n <= 1 else '', source=source)
+    )
+    print('STARTING')
+    print(message)
+    return def end():
+        print('ENDED')
+        print(message)
+
 
 class Command(BaseCommand):
     help = "populate db"
@@ -24,19 +54,14 @@ class Command(BaseCommand):
             "--number", "-n",
             type=int,
             default=10,
-            help="Number of elements to add to the DW"
+            help="Number of elements of each kind to add to the DW"
         )
 
     def handle(self, *args, **options):
         n = options["number"]
-        print("Putting {} element{} from IPREL into DW".format(
-            n, "" if n <= 1 else "s"
-        ))
-        for input in iprel.Entry.objects.using("interpro_ro").all()[:n]:
-            output = Entry(
-                accession=input.entry_ac, type=input.entry_type_id,
-                name=input.name, short_name=input.short_name, other_names=[],
-                member_databases={}, go_terms={},
-                literature=[extractPubs(input.entry2pub_set.all())]
-            )
-            output.save()
+        end = log(n, 'interpro entry object')
+        get_interpro_entries(n)
+        end()
+        end = log(n, 'member db entry object')
+        get_member_db_entries(n)
+        end()
--- release/oracle.py
+++
@@ -1,6 +0,0 @@
-import cx_Oracle
-
-def get_cursor(host, port, name, user, password):
-    dsn = cx_Oracle.makedsn(host, port, name)
-    con = cx_Oracle.connect(user, password, dsn)
-    return con.cursor()

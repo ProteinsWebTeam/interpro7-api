a69e91589ba1e11dcfa67e2b834077593580e90c
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2016-03-16 12:28:07 +0000

    fixing the functional test for proteins

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -57,14 +57,13 @@ class RESTRequestsTest(FunctionalTest):
         jsonp = json.loads(content)
 
         self.assertEqual(len(jsonp), num_uniprot, "The response should have as many entries as reported in /entry ")
-        self.assertIn("metadata", jsonp[0].keys(), "'metadata' should be one of the keys in the response")
-        self.assertIn("molecular_function", jsonp[0]["metadata"]["go_terms"],
-                      "the key is part of the go_terms and has been parsed OK")
+        acc = jsonp[0]["accession"]
 
-        acc = jsonp[0]["metadata"]["accession"]
         self.browser.get(self.server_url + "/api/protein/uniprot/"+acc+"?format=json")
         content = self.browser.find_element_by_tag_name('body').text
 
         jsonp = json.loads(content)
         self.assertEqual(acc, jsonp["metadata"]["accession"],
                          "The accession in the response object should be the same as reequested")
+        self.assertIn("molecular_function", jsonp["metadata"]["go_terms"],
+                      "the key is part of the go_terms and has been parsed OK")

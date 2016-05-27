c36d65a0db286480d2034ca002e2dc6c90f85dca
Aurélien Luciani <luciani@ebi.ac.uk>
2016-05-27 17:03:03 +0100

    change in matches format

--- webfront/serializers/interpro.py
+++ webfront/serializers/interpro.py
@@ -78,8 +78,7 @@ class EntrySerializer(ModelContentSerializer):
     def to_match_representation(match, full=False):
         output = {
             "accession": match.protein_id,
-            "match_start": match.match_start,
-            "match_end": match.match_end,
+            "match": [match.match_start, match.match_end],
             "length": match.protein.length,
             "source_database": match.protein.source_database,
             "name": match.protein.name,

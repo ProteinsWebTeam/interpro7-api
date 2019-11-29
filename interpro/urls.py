9013464ed548789bd4b5f6720c980165936dc292
Swaathik <swaathik@gmail.com>
2019-11-29 10:38:55 +0000

    Mail view URL corrected

--- interpro/urls.py
+++ interpro/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from webfront.views import common, mail
 
 urlpatterns = [
+    url(r"^api/mail/$", mail.mail_interhelp),
     url(r"^api/(?P<url>.*)$", common.GeneralHandler.as_view()),
-    url(r"^mail/$", mail.mail_interhelp)
 ]

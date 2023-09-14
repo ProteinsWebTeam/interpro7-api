523a35240eb39aa3a481aa5f754ef565777cb08a
Matthias Blum <mat.blum@gmail.com>
2023-09-14 14:56:15 +0100

    /api/mail accepts an optional "queue" argument

--- interpro/urls.py
+++ interpro/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from webfront.views import common, mail
 
 urlpatterns = [
-    url(r"^api/mail/$", mail.mail_interhelp),
+    url(r"^api/mail/$", mail.send_email),
     url(r"^api/(?P<url>.*)$", common.GeneralHandler.as_view()),
 ]

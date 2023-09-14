116784466c3f7d7f558b136024657da4f0d8e052
Matthias Blum <mat.blum@gmail.com>
2023-09-14 16:06:26 +0100

    Call time.sleep to avoid 429 error, and add test to emulate 429

--- webfront/tests/test_mail.py
+++ webfront/tests/test_mail.py
@@ -1,10 +1,12 @@
+import time
+
 from django.test import TestCase
 from django.test import Client
 from rest_framework import status
 
 
 class TestMail(TestCase):
-    def test_mail(self):
+    def test_mail(self, sleep=60):
         self.client = Client()
         response = self.client.post(
             "/api/mail/",
@@ -17,6 +19,23 @@ class TestMail(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()["from"], "swaathik@ebi.ac.uk")
+        time.sleep(sleep)
+
+    def test_spam(self):
+        self.test_mail(sleep=0)
+        self.client = Client()
+        response = self.client.post(
+            "/api/mail/",
+            {
+                "path": "echo",
+                "subject": "Add annotation test from API",
+                "message": "Test",
+                "from_email": "swaathik@ebi.ac.uk",
+            },
+        )
+        self.assertEqual(response.status_code,
+                         status.HTTP_429_TOO_MANY_REQUESTS)
+        time.sleep(60)
 
     def test_mail_invalid_queue(self):
         self.client = Client()
@@ -31,3 +50,4 @@ class TestMail(TestCase):
             },
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        time.sleep(60)

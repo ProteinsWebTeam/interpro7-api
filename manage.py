ba9772f4779052398ec2a151d2eb6ed36faec0f2
apolignano <alessandro.polignano@ebi.ac.uk>
2025-08-05 12:02:29 +0200

    Remove MySQL import

--- manage.py
+++ manage.py
@@ -2,16 +2,6 @@
 import os
 import sys
 
-try:
-    import pymysql
-
-    pymysql.version_info = (1, 4, 6, "final", 0)  # change mysqlclient version
-    pymysql.install_as_MySQLdb()
-    print("running pymysql")
-
-except ImportError:
-    pass
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interpro.settings")
 
